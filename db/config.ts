976002e6c0f8e0831f8d999df5a2abce416e889b
khaya-zulu <39437696+khaya-zulu@users.noreply.github.com>
2024-05-03 10:16:14 +0200

    add db with snaplet seed

---
+++ db/config.ts
@@ -0,0 +1,26 @@
+import { defineDb, defineTable, column } from "astro:db";
+
+const Page = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    url: column.text(),
+    imageUrl: column.text(),
+    description: column.text(),
+    createdAt: column.date({ default: new Date() }),
+  },
+});
+
+const Click = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    pageId: column.number({ references: () => Page.columns.id }),
+  },
+});
+
+// https://astro.build/db/config
+export default defineDb({
+  tables: {
+    Page,
+    Click,
+  },
+});
