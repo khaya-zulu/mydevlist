32064b0b62ac45f49655d04da22c977bde101b11
khaya-zulu <39437696+khaya-zulu@users.noreply.github.com>
2026-05-29 18:17:34 +0200

    start project

---
+++ src/app/document.tsx
@@ -0,0 +1,27 @@
+export const Document: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => (
+  <html lang="en">
+    <head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>@redwoodjs/starter-minimal</title>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin=""
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=optional"
+        precedence="first"
+      />
+      <link rel="modulepreload" href="/src/client.tsx" />
+    </head>
+    <body>
+      {children}
+      <script>import("/src/client.tsx")</script>
+    </body>
+  </html>
+);
